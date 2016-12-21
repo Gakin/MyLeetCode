b311c0b07fe8e57b55dde02028277d85ec1ca26b
Goksucan Akin <gakin@usc.edu>
2016-12-21 06:46:21 -0800

    Create 206_Reverse_Linked_List.py

---
+++ 206_Reverse_Linked_List.py
@@ -0,0 +1,10 @@
+#Solution is taken from the leetcode itself because of its easy to understand way of build
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
